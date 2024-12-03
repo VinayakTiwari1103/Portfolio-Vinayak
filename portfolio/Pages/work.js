07d5c9ac86fd56fae166b78aa2baa67632da10a3
Vinayak Tiwari <84216407+VinayakTiwari1103@users.noreply.github.com>
2024-12-03 23:59:55 +0530

    replaced work tab in nav pannel'

---
+++ portfolio/Pages/work.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>Work TAB</h1>
+            <p>Tell your visitors about yourself, your background, and what you do.  You can include a profile picture, skills, and anything else that helps them get to know you.</p>
+        </div>
+    );
+}
